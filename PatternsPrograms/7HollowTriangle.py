52921d185351d58f076c1bd4726f0019cbbdc0f4
sidhu <sdupati4@gmail.com>
2023-05-31 00:31:06 +0530

    rock4

--- PatternsPrograms/7HollowTriangle.py
+++ PatternsPrograms/7HollowTriangle.py
@@ -1,9 +1,12 @@
-print('* ' * 5)
+'''print('* ' * 5)
 for i in range(3):
     print('*   *')
 print('* ' * 5)
 
-'''
+    
 Not working as expected.
 
 '''
+for i in range(1,6):
+    print(" "*(i-1)+"*")
+    
